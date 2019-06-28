a55397d352c2acd94196911c0aaf79a3b83eda55
Diogo Duarte <dduarte@odoogap.com>
2019-06-28 12:27:22 +0100

    [12.0][MIG] l10n_us_gaap: Migration to 12.0

--- l10n_us_gaap/__manifest__.py
+++ l10n_us_gaap/__manifest__.py
@@ -1,16 +1,18 @@
 {
     'name': 'United States Sample GAAP Chart of Accounts',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
+    'development_status': 'Mature',
     'category': 'Localization',
     'author': 'Eficent,'
               'Odoo Community Association (OCA)',
+    'maintainers': ['jbeficent'],
     'website': 'https://github.com/OCA/l10n-usa/',
     'depends': ['account'],
     'data': [
         'data/account_chart_template_data.xml',
         'data/account_group.xml',
         'data/account_us_gaap_data.xml',
-        'data/account_chart_template_data.yml',
+        'data/account_chart_template_data_load.xml',
         'data/res_company_data.xml',
     ],
     'installable': True,
