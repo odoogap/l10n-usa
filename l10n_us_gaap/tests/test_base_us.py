1bfb424fe421bc2ed9069725bf8e3d575036ef05
Diogo Duarte <dduarte@odoogap.com>
2019-07-14 22:11:51 +0100

    [IMP] adding first unit tests

---
+++ l10n_us_gaap/tests/test_base_us.py
@@ -0,0 +1,16 @@
+from odoo.tests.common import TransactionCase
+from odoo.tests import tagged
+
+
+@tagged('post_install', '-at_install')
+class TestChartAcount(TransactionCase):
+
+    def test_basic(self):
+        chart_us = self.env.ref('l10n_us_gaap.l10n_us_gaap_chart_template')
+        chart_us.try_loading_for_current_company()
+        bank_account = self.env['account.account'].search([('code', '=', '111101')])
+        liquidity_transfer = self.env['account.account'].search([('code', '=', '111901')])
+        finished_goods = self.env['account.account'].search([('code', '=', '133000')])
+        self.assertEqual(bank_account.name, 'Bank')
+        self.assertEqual(liquidity_transfer.name, 'Liquidity Transfer')
+        self.assertEqual(finished_goods.name, 'Finished Goods')
