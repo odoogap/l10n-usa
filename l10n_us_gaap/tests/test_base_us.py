00cbada16071ffec02b417f7df4a839fe00e6cca
Diogo Duarte <dduarte@odoogap.com>
2019-07-27 18:47:56 +0100

    Update l10n_us_gaap/tests/test_base_us.py
    Co-Authored-By: Bhavesh Odedra <bodedra@opensourceintegrators.com>

--- l10n_us_gaap/tests/test_base_us.py
+++ l10n_us_gaap/tests/test_base_us.py
@@ -8,7 +8,8 @@ class TestChartAcount(TransactionCase):
     def test_basic(self):
         chart_us = self.env.ref('l10n_us_gaap.l10n_us_gaap_chart_template')
         chart_us.try_loading_for_current_company()
-        bank_account = self.env['account.account'].search([('code', '=', '111101')])
+        bank_account = self.env['account.account'].search(
+            [('code', '=', '111101')])
         liquidity_transfer = self.env['account.account'].search([('code', '=', '111901')])
         finished_goods = self.env['account.account'].search([('code', '=', '133000')])
         self.assertEqual(bank_account.name, 'Bank')
