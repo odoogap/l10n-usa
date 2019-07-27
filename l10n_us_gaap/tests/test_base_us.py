f54feb3a0595c6d824adcb743a447d9fcb815803
Diogo Duarte <dduarte@odoogap.com>
2019-07-27 18:48:11 +0100

    Update l10n_us_gaap/tests/test_base_us.py
    Co-Authored-By: Bhavesh Odedra <bodedra@opensourceintegrators.com>

--- l10n_us_gaap/tests/test_base_us.py
+++ l10n_us_gaap/tests/test_base_us.py
@@ -2,7 +2,7 @@ from odoo.tests.common import TransactionCase
 from odoo.tests import tagged
 
 
-@tagged('post_install', '-at_install')
+@tagged('post_install')
 class TestChartAcount(TransactionCase):
 
     def test_basic(self):
@@ -12,7 +12,8 @@ class TestChartAcount(TransactionCase):
             [('code', '=', '111101')])
         liquidity_transfer = self.env['account.account'].search(
             [('code', '=', '111901')])
-        finished_goods = self.env['account.account'].search([('code', '=', '133000')])
+        finished_goods = self.env['account.account'].search(
+            [('code', '=', '133000')])
         self.assertEqual(bank_account.name, 'Bank')
         self.assertEqual(liquidity_transfer.name, 'Liquidity Transfer')
         self.assertEqual(finished_goods.name, 'Finished Goods')
